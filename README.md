2a8529142fa49f9e42dba3e5dc5779a2325046b5
Jean <login@jeanzanchetta.com>
2025-07-23 03:10:26 +0000

    adiciona script `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # project-tabnews
+
 project tabnews
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1> Você é massinha </h1>
+function Home() {
+  return <h1> Você é massinha </h1>;
 }
 
 export default Home;
